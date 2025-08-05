6b879da29c0727539bf8b81dacbe1f4f420fe6da
yusufbaltamegacode <yusuf.balta@megacode.com.tr>
2025-08-05 16:35:49 +0300

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,8 +17,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   /// 默认使用了再惠合伙人的下载地址
-  static const _defaultUrl =
-      'https://itunes.apple.com/cn/app/%E5%86%8D%E6%83%A0%E5%90%88%E4%BC%99%E4%BA%BA/id1375433239?l=zh&ls=1&mt=8';
+  static const _defaultUrl = 'https://itunes.apple.com/cn/app/%E5%86%8D%E6%83%A0%E5%90%88%E4%BC%99%E4%BA%BA/id1375433239?l=zh&ls=1&mt=8';
   TextEditingController _textEditingController = TextEditingController();
   String _appUrl = '';
   double _progressValue = 0.0;
@@ -50,8 +49,7 @@ class _MyAppState extends State<MyApp> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Padding(
-                      padding:
-                          const EdgeInsets.only(top: 30, left: 16.0, right: 16),
+                      padding: const EdgeInsets.only(top: 30, left: 16.0, right: 16),
                       child: LinearProgressIndicator(
                         value: _progressValue,
                         backgroundColor: Colors.grey,
@@ -59,23 +57,15 @@ class _MyAppState extends State<MyApp> {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(
-                          top: 16, left: 16.0, right: 16, bottom: 16),
+                      padding: const EdgeInsets.only(top: 16, left: 16.0, right: 16, bottom: 16),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                              'downloading ${(_progressValue * 100).toStringAsFixed(0)} %')
-                        ],
+                        children: [Text('downloading ${(_progressValue * 100).toStringAsFixed(0)} %')],
                       ),
                     ),
-                    ElevatedButton(
-                        onPressed: () => _networkInstallApk(),
-                        child: Text('network install apk')),
+                    ElevatedButton(onPressed: () => _networkInstallApk(), child: Text('network install apk')),
                     SizedBox(height: 10),
-                    ElevatedButton(
-                        onPressed: () => _localInstallApk(),
-                        child: Text('local install apk')),
+                    ElevatedButton(onPressed: () => _localInstallApk(), child: Text('local install apk')),
                   ],
                 )
               : Column(
@@ -84,16 +74,11 @@ class _MyAppState extends State<MyApp> {
                     Platform.isIOS
                         ? TextField(
                             controller: _textEditingController,
-                            decoration: InputDecoration(
-                                hintText: 'URL for app store to launch'),
+                            decoration: InputDecoration(hintText: 'URL for app store to launch'),
                             onChanged: (url) => _appUrl = url,
                           )
                         : SizedBox(),
-                    Platform.isIOS
-                        ? ElevatedButton(
-                            onPressed: () => _gotoAppStore(_appUrl),
-                            child: Text('gotoAppStore'))
-                        : SizedBox()
+                    Platform.isIOS ? ElevatedButton(onPressed: () => _gotoAppStore(_appUrl), child: Text('gotoAppStore')) : SizedBox()
                   ],
                 ),
         ),
@@ -110,8 +95,7 @@ class _MyAppState extends State<MyApp> {
     _progressValue = 0.0;
     var appDocDir = await getTemporaryDirectory();
     String savePath = appDocDir.path + "/zaihui_kylinim_42.apk";
-    String fileUrl =
-        "https://s3.cn-north-1.amazonaws.com.cn/mtab.kezaihui.com/apk/kylinim/zaihui_kylinim_42.apk";
+    String fileUrl = "https://s3.cn-north-1.amazonaws.com.cn/mtab.kezaihui.com/apk/kylinim/zaihui_kylinim_42.apk";
     await Dio().download(fileUrl, savePath, onReceiveProgress: (count, total) {
       final value = count / total;
       if (_progressValue != value) {
@@ -127,16 +111,14 @@ class _MyAppState extends State<MyApp> {
     });
 
     final res = await InstallPlugin.install(savePath);
-    _showResMsg(
-        "install apk ${res['isSuccess'] == true ? 'success' : 'fail:${res['errorMessage'] ?? ''}'}");
+    _showResMsg("install apk ${res['isSuccess'] == true ? 'success' : 'fail:${res['errorMessage'] ?? ''}'}");
   }
 
   _localInstallApk() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
     if (result != null) {
       final res = await InstallPlugin.install(result.files.single.path ?? '');
-      _showResMsg(
-          "install apk ${res['isSuccess'] == true ? 'success' : 'fail:${res['errorMessage'] ?? ''}'}");
+      _showResMsg("install apk ${res['isSuccess'] == true ? 'success' : 'fail:${res['errorMessage'] ?? ''}'}");
     } else {
       // User canceled the picker
       _showResMsg("User canceled the picker apk");
@@ -146,8 +128,7 @@ class _MyAppState extends State<MyApp> {
   _gotoAppStore(String url) async {
     url = url.isEmpty ? _defaultUrl : url;
     final res = await InstallPlugin.install(url);
-    _showResMsg(
-        "go to appstroe ${res['isSuccess'] == true ? 'success' : 'fail:${res['errorMessage'] ?? ''}'}");
+    _showResMsg("go to appstroe ${res['isSuccess'] == true ? 'success' : 'fail:${res['errorMessage'] ?? ''}'}");
   }
 
   _showResMsg(String msg) {
